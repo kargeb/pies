ca7b498549cfd4035fe19f1e714106d72ebb8f8e
karol gebarowski <karol.gebarowski@itbees.pl>
2026-02-16 21:56:19 +0100

    routes

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -3,18 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { Topbar } from './core/layout/topbar/topbar';
+import { Sidebar } from './core/sidebar/sidebar';
+
+const coreComponents = [Topbar, Sidebar];
 
 @NgModule({
-  declarations: [
-    App
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [
-    provideBrowserGlobalErrorListeners()
-  ],
-  bootstrap: [App]
+  declarations: [App],
+  imports: [BrowserModule, AppRoutingModule, coreComponents],
+  providers: [provideBrowserGlobalErrorListeners()],
+  bootstrap: [App],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Home } from './features/home/home';
+import { Users } from './features/users/users';
+import { Todo } from './features/todo/todo';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: Home,
+  },
+  // { path: 'home', component: undefined },
+  { path: 'users', component: Users },
+  { path: 'todo', component: Todo },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  // { path: '**', component: undefined },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
