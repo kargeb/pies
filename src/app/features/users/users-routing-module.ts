2580f236e6692f1341426364196dfc81ee0f232b
karol gebarowski <karol.gebarowski@itbees.pl>
2026-02-17 18:18:14 +0100

    users child routing buttons

--- src/app/features/users/users-routing-module.ts
+++ src/app/features/users/users-routing-module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Users } from './users';
 import { AllUsers } from './components/all-users/all-users';
+import { UserDetails } from './components/user-details/user-details';
+import { AddUser } from './components/add-user/add-user';
 
 const routes: Routes = [
   {
@@ -9,8 +11,8 @@ const routes: Routes = [
     component: Users,
     children: [
       { path: 'all-users', component: AllUsers },
-      //   { path: 'user-details', component: undefined },
-      //   { path: 'add-suer', component: undefined },
+      { path: 'user-details', component: UserDetails },
+      { path: 'add-user', component: AddUser },
       { path: '', redirectTo: 'all-users', pathMatch: 'full' },
     ],
   },
--- src/app/features/users/users.ts
+++ src/app/features/users/users.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.html',
   styleUrl: './users.scss',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
 })
 export class Users {}
