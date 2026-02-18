aa5161939bbf5a2d313c60efcf3ff4da1932f1a0
karol gebarowski <karol.gebarowski@itbees.pl>
2026-02-18 17:52:30 +0100

    route param with diferent methods

--- src/app/features/users/components/user-details/user-details.ts
+++ src/app/features/users/components/user-details/user-details.ts
@@ -1,5 +1,6 @@
 import {
   ChangeDetectionStrategy,
+  ChangeDetectorRef,
   Component,
   computed,
   effect,
@@ -8,7 +9,8 @@ import {
   Signal,
 } from '@angular/core';
 import { takeUntilDestroyed, toSignal } from '@angular/core/rxjs-interop';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { map, Observable, of, Subscription, switchMap, take, tap } from 'rxjs';
 
 @Component({
   selector: 'app-user-details',
@@ -19,10 +21,14 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class UserDetails {
   private readonly activatedRoute = inject(ActivatedRoute);
+  private readonly cdr = inject(ChangeDetectorRef);
+  private subscription: Subscription = new Subscription();
 
-  public userId?: number;
+  public userId?: number | string;
 
   private paramsSignal = toSignal(this.activatedRoute.paramMap);
+
+  // private observableParams?: Observable<ParamMap> = this.activatedRoute.paramMap;
 
   public userId2: Signal<number> = signal(0);
 
@@ -34,8 +40,39 @@ export class UserDetails {
 
   public ngOnInit(): void {
     // this.userId = Number(this.paramsSignal()?.get('id'));
-    this.userId2 = computed(() => Number(this.paramsSignal()?.get('id')));
+    // this.userId2 = computed(() => Number(this.paramsSignal()?.get('id')));
     console.log('userId: ', this.userId);
+    this.subscription.add(
+      this.activatedRoute.params
+        .pipe(
+          map((params) => {
+            console.log('params from map:', params);
+            this.userId = params['id'];
+            console.log('this.userrr: ', this.userId);
+            this.cdr.markForCheck();
+            return this.userId;
+            // return of(this.userId);
+          }),
+          switchMap((userIdObserv) => {
+            // const valueFromObservable = toSignal(userIdObserv);
+
+            console.log('signal value from obserab+le: ', userIdObserv);
+            // console.log(
+            //   'userIdObserv: ',
+            //   userIdObserv
+            //     .pipe(
+            //       take(1),
+            //       tap((value) => {
+            //         console.log('Value from obsevable:', value);
+            //       }),
+            //     )
+            //     .subscribe(),
+            // );
+            return of(userIdObserv);
+          }),
+        )
+        .subscribe(),
+    );
   }
 
   private getIdBySubscribe() {
@@ -47,5 +84,7 @@ export class UserDetails {
       });
   }
 
-  public ngOnDestroy() {}
+  public ngOnDestroy() {
+    this.subscription?.unsubscribe();
+  }
 }
