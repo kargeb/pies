a3ab7a63bdc0c17e7951f7edd4c8c962094239b6
karol gebarowski <karol.gebarowski@itbees.pl>
2026-02-23 17:05:34 +0100

    errors in form

--- src/app/features/users/containers/add-user/add-user.ts
+++ src/app/features/users/containers/add-user/add-user.ts
@@ -1,11 +1,72 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-add-user',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './add-user.html',
   styleUrl: './add-user.scss',
 })
 export class AddUser {
+  private readonly fb = inject(FormBuilder);
 
+  public usernameMaxLength = 10;
+
+  // public form = this.fb.nonNullable.group({
+  public form = this.fb.group({
+    username: [
+      '',
+      [Validators.required, Validators.maxLength(this.usernameMaxLength)],
+    ],
+    age: [0, [Validators.required, Validators.max(200)]],
+    city: ['', [Validators.required]],
+    // assets: this.[],
+  });
+
+  //   ngOnInit() {
+  //  this.form = this.fb.group({
+  //   username: '',
+  //   age: new FormControl<number | string>(0),
+  //   city: '',
+  //   assets: [],
+  // });
+  //   }
+
+  ngOnInit() {
+    this.fillForm();
+  }
+
+  public fillForm() {
+    this.form?.patchValue({
+      age: 10,
+    });
+  }
+
+  public showForm() {
+    console.log('form: ', this.form.value);
+  }
+
+  public resetForm() {
+    this.form.reset();
+  }
+
+  public submit() {
+    console.log('form: ', this.form.value);
+  }
+
+  public get username(): FormControl {
+    return this.form.controls.username;
+  }
+  public get city(): FormControl {
+    return this.form.controls.city;
+  }
+  public get age(): FormControl {
+    return this.form.controls.age;
+  }
 }
